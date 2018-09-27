298663af4192168cc34022ba87c48c4a29d6f22a
sirfoga <sirfoga@protonmail.com>
2018-09-27 19:09:30 +0200

    remove copyright

--- bot/bot.py
+++ bot/bot.py
@@ -1,26 +1,11 @@
 # !/usr/bin/python
 # coding: utf_8
 
-# Copyright 2016-2017 Race UP
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 import argparse
 import base64
 import csv
 import datetime
-import locale
 import os
 
 from hal.internet.email import gmail
@@ -56,8 +41,9 @@ EMAIL_HEADER_FILE = os.path.join(DATA_FOLDER, "email_header.txt")
 EMAIL_CONTENT_FILE = os.path.join(DATA_FOLDER, "email_content.txt")
 EMAIL_FOOTER_FILE = os.path.join(DATA_FOLDER, "email_footer.txt")
 
-# setting locale
-locale.setlocale(locale.LC_ALL, "it_IT.UTF-8")  # italian
+
+# setting locale to get days in native language
+# locale.setlocale(locale.LC_ALL, "it_IT.UTF-8")  # italian
 
 
 class CakeRemainder(EmailTemplate):
